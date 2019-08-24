538a71cd115ca30358c92980a066b51b515b5a07
Pranav <thepranavtotla@gmail.com>
2019-08-24 17:13:40 +0530

    Removed React Hooks.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Abc', age: 23},
+      { name: 'Lmn', age: 24},
+      { name: 'Zyx', age: 25}
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'AbcIjk', age: 23},
+        { name: 'Lmn', age: 24},
+        { name: 'Zyx', age: 26}
+      ]
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app.</h1>
+        <p>This works too!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: cooking</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+export default App;
+
+/*
 const App = (props) => {
   const [personState, setPersonState] = useState({
     persons: [
@@ -21,7 +58,7 @@ const App = (props) => {
     })
   };
 
-  //render() {
+ 
     return (
       <div className="App">
         <h1>Hi, I'm a react app.</h1>
@@ -32,26 +69,6 @@ const App = (props) => {
         <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
       </div>
     );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
- // }
+    
 }
-
-export default App;
-
-/*state = {
-  persons: [
-    { name: 'Abc', age: 23},
-    { name: 'Lmn', age: 24},
-    { name: 'Zyx', age: 25}
-  ]
-};
-
-switchNameHandler = () => {
-  this.setState({
-    persons: [
-      { name: 'AbcIjk', age: 23},
-      { name: 'Lmn', age: 24},
-      { name: 'Zyx', age: 26}
-    ]
-  });
-}*/
+*/
