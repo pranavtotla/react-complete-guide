197a400294743e1a3794e7cc976548d52e566f76
Pranav <thepranavtotla@gmail.com>
2019-08-24 15:40:12 +0530

    Using JSX for components.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    /*return (
+    return (
       <div className="App">
         <h1>Hi, I'm a react app.</h1>
+        <p>This works too!</p>
       </div>
-    );*/
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
