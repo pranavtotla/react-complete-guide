d7c1a77a7af25f2259fe8a3a4b3accf848666c63
Pranav Totla <thepranavtotla@gmail.com>
2019-08-25 13:46:38 +0530

    Preferred way of conditionals.

--- src/App.js
+++ src/App.js
@@ -49,19 +49,25 @@
         cursor: 'pointer'
       }
 
+      let persons = null;
+
+      if(this.state.showPersons){
+        persons = (
+          <div>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'withBind')} changed={this.nameChangeHandler}>Hobbies: cooking</Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div> 
+        );
+      }
+
 
       return (
         <div className="App">
           <h1>Hi, I'm a react app.</h1>
           <p>This works too!</p>
           <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
-          {this.state.showPersons?
-            <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'withBind')} changed={this.nameChangeHandler}>Hobbies: cooking</Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-            </div> : null
-          }
+          {persons}
         </div>
       );
       //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
