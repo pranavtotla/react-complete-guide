50ceb151631b8c44d8065df934748077fc96872c
Pranav Totla <thepranavtotla@gmail.com>
2019-09-21 17:55:33 +0530

    Updated Persons.js as class bases component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const persons = props => props.persons.map((person, index) => {
-        return <Person 
-                name={person.name} 
-                age={person.age} 
-                click={() => {props.clicked(index)}}
-                key={person.id}
-                changed={(event) =>{props.changed(event, person.id)}}
-              />
-      });
+class Persons extends Component {
+        render() {
+                return this.props.persons.map((person, index) => {
+                        return (<Person 
+                        name={person.name} 
+                        age={person.age} 
+                        click={() => {this.props.clicked(index)}}
+                        key={person.id}
+                        changed={(event) =>{this.props.changed(event, person.id)}}
+                        />);
+                });
+        }
+}
 
-export default persons;
+export default Persons;
+
+/*const persons = props => props.persons.map((person, index) => {
+        return <Person 
+        name={person.name} 
+        age={person.age} 
+        click={() => {props.clicked(index)}}
+        key={person.id}
+        changed={(event) =>{props.changed(event, person.id)}}
+        />
+});
+
+export default persons;*/
