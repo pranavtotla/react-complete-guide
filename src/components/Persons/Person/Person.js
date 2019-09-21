5c36f27522b60617c1c6f26ee4f3f7e963957b3d
Pranav Totla <thepranavtotla@gmail.com>
2019-09-21 17:48:47 +0530

    Person.js in class based component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
+class Person extends Component {
+    render(){
+        console.log("Rendering Person.js");
+        return <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm {this.props.name}. I'm {this.props.age} years old.</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange = {this.props.changed} value={this.props.name}/>
+    </div>
+    }
+}
+
+
+/*const person = (props) => {
     return(
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name}. I'm {props.age} years old.</p>
@@ -9,6 +21,6 @@ const person = (props) => {
             <input type="text" onChange = {props.changed} value={props.name}/>
         </div>
     );
-};
+};*/
 
-export default person;
+export default Person;
