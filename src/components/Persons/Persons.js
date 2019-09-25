48f204c317e5bcf2466b556592408c29e26084ef
Pranav <thepranavtotla@gmail.com>
2019-09-25 17:21:48 +0530

    Reformatting

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -9,6 +9,6 @@ const persons = props => props.persons.map((person, index) => {
                 key={person.id}
                 changed={(event) =>{props.changed(event, person.id)}}
               />
-      });
+});
 
 export default persons;
